2e1dcac253ec791efbde5bfd4d1d52d2653d58af
Ahemd <ahmedsaye23156@gmail.com>
2023-03-07 12:09:55 +0200

    new other wolrd

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// Setup
-const myArray = [["John", 23], ["cat", 2]];
-myArray.push["dog", 3]
-// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
