19912a3a4e3bd58785cc21eb5f2963a3893c612b
Ahemd <ahmedsaye23156@gmail.com>
2023-03-07 11:43:51 +0200

    adding a new function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const sandwich = ["peanut butter", "jelly", "bread"];
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push["dog", 3]
 // Only change code below this line
-const myArray = ["Ahmed", 2, "Hassan"];
