1b966db809482de61e511ebe7b44c7279646e4a7
Ahemd <ahmedsaye23156@gmail.com>
2023-03-07 12:33:00 +0200

    new lines

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-function functionWithArgs(one, two) {
-    console.log(one + two)
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal here
+    oopsGlobal = 5;
 }
 
-functionWithArgs(1, 2);
-functionWithArgs(7, 9);
+// Only change code above this line
+
+function fun2() {
+    let output = "";
+    if (typeof myGlobal != "undefined") {
+        output += `myGlobal: ${myGlobal} \n `;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += `oopsGlobl : ${oopsGlobal}`;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
