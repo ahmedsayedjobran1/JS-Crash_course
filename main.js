ff66bf166a70e082af9a08c3555066e542200e01
Ahemd <ahmedsaye23156@gmail.com>
2023-03-08 23:45:13 +0200

    commiting the value of the nested function

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function timeRemaining(timeElapsed, end) {
-    return end - timeElapsed
+function addfavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+        favoriteBooks.push(bookName)
+    }
 }
-var left = timeRemaining(10, 5);
-
-console.log(left);
+function printFavoriteBooks() {
+    console.log(
+        `favorite Books: ${favoriteBooks.length}`
+    );
+    for (let bookName of favoriteBooks) {
+        console.log(bookName);
+    }
+    // this is a function that prints the arguments in the loops
+}
+var favoriteBooks = [];
+addfavoriteBook("A song of Ice  and Fire");
+addfavoriteBook("The Grear  Gatsby");
+addfavoriteBook("Crime & Punishment");
+addfavoriteBook("Great Expections");
+addfavoriteBook("You Don't Know JS");
+console.log(favoriteBooks);
+printFavoriteBooks();
+// TODO: print out favorite books;
