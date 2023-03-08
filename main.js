c83809d4c31dc7b930a6f6322f797c5dc8453ed4
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 00:15:03 +0200

    new area

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // three Pillars of JS
-console.log(typeof (null));
+// console.log(typeof (null));
+var v;  //undefined
+console.log(typeof v);
+v = function () { };  // function
+console.log(typeof v);
+v = [1, 2, 3]; // object
+console.log(typeof v);
+
+console.log(typeof (function () { }) + "I'm here");
