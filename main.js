17f57879cdc9a07810a1065d1e420923b656b245
Ahemd <ahmedsaye23156@gmail.com>
2023-03-08 14:26:14 +0200

    new areas

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-let myFreinds = ["Hajar", "Nada", "Yosra", "Lyla", "Hema"];
-while (myFreinds.length > 0) {
-    let A11 = myFreinds.shift();
-    sortIt(A11);
+let student = {
+    name: "Ahmed",
+    age: 21,
+    location: "Aswan"
 }
-function sortIt(x) {
-    console.log(`${x} nice to meet you it's a long time since we meet`)
+function into() {
+    console.log(`${student.name} nice to meet you age:${student.age} ${student.location}/:Aswan`);
 }
-
-
-// for (let i = 0; i < myFreinds.length; i++) {
-//     CheckIt(myFreinds[i]);
-// }
-// function CheckIt(x) {
-//     console.log(`hi ${x} nice to meet you`);
-// }
+into();
