c827693f504228b212fa21aa60926818c890ee2c
Ahemd <ahmedsaye23156@gmail.com>
2023-03-08 14:28:12 +0200

    new othre world

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 let student = {
-    name: "Ahmed",
+    name: "sara",
     age: 21,
     location: "Aswan"
+}
+
+if (student.name === "Ahmed") {
+    into();
+}
+else {
+    console.log("Ahmed is the geniest boy in the world")
 }
 function into() {
     console.log(`${student.name} nice to meet you age:${student.age} ${student.location}/:Aswan`);
 }
-into();
+// into();
