f19736ea23b5e4fe7de6ef763afe6bdee026861d
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 00:56:40 +0200

    you can use !! marks as a boolen value

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let one = "Dreams ";
+let one = "1 ";
 let two = "Come true";
-let num = 1;
-let sum = `${one}${two} ${num + ""}`;
-console.log(typeof sum);
-console.log(`${typeof num + ""}`);
+let num = 20;
+let inner = Number(one);
+// when you added a new double quotes lines it will give you a head of comments here  iudf vb v  v cjsa you have to be able to make the nuber in a string type or a historical method
+if (num.valueOf()) {
+    let sum2 = num.valueOf() + 20;
+    console.log(sum2);
+}
+
+
