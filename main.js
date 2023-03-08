bdb7981a60a3d460e6d3d27415fee9a12f614ecb
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 00:19:24 +0200

    adding a bolean value as a NaN

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-// three Pillars of JS
-// console.log(typeof (null));
-var v;  //undefined
-console.log(typeof v);
-v = function () { };  // function
-console.log(typeof v);
-v = [1, 2, 3]; // object
-console.log(typeof v);
-
-console.log(typeof (function () { }) + "I'm here");
+let name = "Ahmed";
+var something = name / 2;
+console.log(Number.isNaN(something));
+// if you want to test something if it's is a "NAN" or not you can do it  Number.isNaN() // it returns a boolean value
