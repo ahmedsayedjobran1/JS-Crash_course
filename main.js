63a84df1f68cb49eafa75ccee5a6d87122038452
Ahemd <ahmedsaye23156@gmail.com>
2023-03-08 12:29:33 +0200

    new variables

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log(typeof(Object));
+console.log(typeof 42);
+console.log(typeof "kyle");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof { age: 39 });
+console.log(typeof null);
+console.log(typeof [1, 3, 4]);   
