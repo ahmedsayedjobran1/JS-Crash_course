7bd7dc60e83d632b6525c77b40c059c4de23a078
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 11:40:19 +0200

    adding a commit

--- JS_noo/main.js
+++ JS_noo/main.js
@@ -1,4 +1,11 @@
-for (var i = 0; i <= 9; i = i + 1) {
-    console.log(i);
+var amount = 99.99;
+function printAmount() {
+    console.log(amount.toFixed(2));
 }
-// 0 1 2 3 4 5 6 7 8 9
+
+
+printAmount(); // "99.99"
+
+amount = amount * 2;
+
+printAmount(); // "199.98"
