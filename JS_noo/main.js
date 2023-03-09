bc18c61f281897ce4eafbbc186d05ccbbf44d9c5
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 11:50:08 +0200

    new other lines

--- JS_noo/main.js
+++ JS_noo/main.js
@@ -1,7 +1,17 @@
-function testA(num) {
-    num = num * 2 + 10;
-    return num;
+function outer() {
+    var a = 1;
+
+    function inner() {
+        var b = 2;
+
+        // we can access both `a` and `b` here
+        console.log(a + b);	// 3
+    }
+
+    inner();
+
+    // we can only access `a` here
+    console.log(a);			// 1
 }
-let students = 10;
-let Fun = students * 2;
-console.log(testA(Fun));
+
+outer();
