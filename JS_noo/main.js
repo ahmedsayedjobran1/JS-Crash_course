70e3ec4635a828adfd392a672fcc787c33e2cb61
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 11:26:30 +0200

    adding amount value

--- JS_noo/main.js
+++ JS_noo/main.js
@@ -1,6 +1,17 @@
+const ACCESSORY_PRICE = 9.99;
+
 var bank_balance = 302.13;
 var amount = 99.99;
 
+amount = amount * 2;
+
+// can we afford the extra purchase?
 if (amount < bank_balance) {
-    console.log("I want to buy this phone!");
+    console.log("I'll take the accessory!");
+    amount = amount + ACCESSORY_PRICE;
+    console.log(`amount:${amount}`);
+}
+// otherwise:
+else {
+    console.log("No, thanks.");
 }
