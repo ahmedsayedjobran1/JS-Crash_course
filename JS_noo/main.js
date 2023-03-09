14f2654ceab6d2d90a598aa2a301c0f857cf04b4
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 11:03:54 +0200

    adding a toFixed file

---
+++ JS_noo/main.js
@@ -0,0 +1,10 @@
+var TAX_RATE = 0.08;	// 8% sales tax
+
+var amount = 99.99;
+
+amount = amount * 2;
+
+amount = amount + (amount * TAX_RATE);
+
+console.log(amount);				// 215.9784
+console.log(amount.toFixed());	// "215.98"
