19d3802858186fa4227ab257a956c72dca476d98
Ahemd <ahmedsaye23156@gmail.com>
2023-03-09 10:17:00 +0200

    adding the first code

--- JS_noo/main.js
+++ JS_noo/main.js
@@ -1,11 +1,5 @@
-let one = "1 ";
-let two = "Come true";
-let num = 21;
-let inner = Number(one);
-// when you added a new double quotes lines it will give you a head of comments here  iudf vb v  v cjsa you have to be able to make the nuber in a string type or a historical method
-if (!!num.valueOf()) {
-    let inner = num.valueOf() - 20;
-    console.log(inner);
-}
+a = 21;
 
+b = a * 2;
 
+console.log(b);
