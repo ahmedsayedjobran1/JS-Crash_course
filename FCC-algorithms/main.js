da73dbdf62cbe6b868958268386e46f2cbf1ff59
Ahemd <ahmedsaye23156@gmail.com>
2023-03-14 09:07:13 +0200

    new other line

--- FCC-algorithms/main.js
+++ FCC-algorithms/main.js
@@ -1,7 +1,11 @@
-// var magic = function() {
-//   return new Date();
+// var myConcat = function(arr1, arr2) {
+//   return arr1.concat(arr2);
 // };
-
-const magic = () => {
-    return new Date();
+let arr1 = [1, 2];
+let arr2 = [3, 4, 5];
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
 }
+myConcat(arr1, arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
