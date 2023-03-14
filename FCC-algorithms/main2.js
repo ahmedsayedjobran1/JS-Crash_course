8ae39839f0bd3e44ab4a2d9018ed6a0cbf19d1e3
Ahemd <ahmedsaye23156@gmail.com>
2023-03-14 09:26:26 +0200

    all is weel

--- FCC-algorithms/main2.js
+++ FCC-algorithms/main2.js
@@ -1,5 +1,10 @@
-// Only change code below this line
-const increment = (number, value=1) => number + value;
-// Only change code above this line
+const sum = (...args) => {
 
-console.log(increment(5));
+    let sum1 = 5;
+    for (let i = 0; i < args.length; i++) {
+        sum1 += args[i];
+    }
+    return sum1
+}
+
+console.log(`your answer is: ${sum(1, 2, 7-5)}`);
