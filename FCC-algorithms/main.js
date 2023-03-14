73438ea467966cbef602e710357f7e0252000c48
Ahemd <ahmedsaye23156@gmail.com>
2023-03-14 09:01:00 +0200

    Ohh i want to be here all over the world

--- FCC-algorithms/main.js
+++ FCC-algorithms/main.js
@@ -1,16 +1,7 @@
-function freezeObj() {
-    const MATH_CONSTANTS = {
-        PI: 3.14
-    };
-    // Only change code below this line
-    // Object.freeze(MATH_CONSTANTS);
+// var magic = function() {
+//   return new Date();
+// };
 
-    // Only change code above this line
-    try {
-        MATH_CONSTANTS.PI = 99;
-    } catch (ex) {
-        console.log(ex);
-    }
-    console.log(MATH_CONSTANTS.PI);
+const magic = () => {
+    return new Date();
 }
-const PI = freezeObj();
