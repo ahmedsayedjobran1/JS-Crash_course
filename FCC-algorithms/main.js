bdc01ab44be54d61502394968cc279977720222e
Ahemd <ahmedsaye23156@gmail.com>
2023-03-14 08:57:52 +0200

    all is well

--- FCC-algorithms/main.js
+++ FCC-algorithms/main.js
@@ -1,12 +1,16 @@
-const s = [5, 7, 2];
-function editInPlace() {
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
     // Only change code below this line
+    // Object.freeze(MATH_CONSTANTS);
 
-    // Using s = [2, 5, 7] would be invalid
-    s[0] = 2;
-    s[1] = 5;
-    s[2] = 7;
-    console.log(s);
     // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    console.log(MATH_CONSTANTS.PI);
 }
-editInPlace();
+const PI = freezeObj();
